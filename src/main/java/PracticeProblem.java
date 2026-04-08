f80801f128d85de353dbe7809280d377f9c778dd
Alfred-Yang-ICD2O1-02 <alfred.yang@student.tdsb.on.ca>
2026-04-08 15:49:46 +0000

    ubifwa

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,13 +1,61 @@
+/**
+
+        * File: Lesson 3.5 - Problem Solving and Good Programming Style
+
+        * Author: Alfred Yang
+
+        * Date Created: April 8, 2026
+
+        * Date Last Modified: April 8, 2026
+
+        */
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		System.out.println(getFirstName("Mary Jane Watson"));
+		System.out.println(getFirstName("  Alice Smith"));
+		System.out.println(getLastName("John Doe"));
+		System.out.println(isValidName("John"));
 	}
 
 	public static void q1() {
 		//Write question 1 code here
 	}
+	public static String getFirstName(String buh){
+		String firstName;
+		buh = buh.trim();
+		int space = buh.indexOf(" ");
+		firstName = buh.substring(0, space); 
+		return firstName;
+		}
 
+	public static String getLastName(String buh){
+		String lastName;
+		buh = buh.trim();
+		int space = buh.lastIndexOf(" ");
+		lastName = buh.substring(space+1); 
+		return lastName;
+		}
+
+		public static Boolean isValidName(String buh){
+			buh = buh.trim();
+			int space = buh.indexOf(" ");
+			if (!(buh.contains(" "))){
+				return false;
+			}
+			String first = buh.substring(0, space);
+			String last = buh.substring(space+1);
+
+			if (first.contains(" ") || last.contains(" ")){
+				return false;
+			}
+			else if (first.length() < 2 || last.length() < 2){
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
 	public static void q2() {
 		//Write question 2 code here
 	}
